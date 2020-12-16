a4f1b06a25d3bbbbb0c714f4c50543451aed4b6b
Starch <mrdave67@gmail.com>
2020-12-16 13:05:16 +0000

    more work on responsiveness

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,18 +7,18 @@ export class Footer extends React.Component {
     render(){
         return(
             <div id='footer'>
-            <Nav footer={true}/>
-            <div id='footer-social'>
+            <Nav footer={true} />
+            <div id='footer-social' className='footerUL'>
                 <ul>
-                    <a href='#'><li>Facebook</li></a>
-                    <a href='#'><li>Instagram</li></a>
+                    <a href='#' target='_blank'><li>Facebook</li></a>
+                    <a href='https://www.instagram.com/thegingerblondiebakery/' target='_blank'><li>Instagram</li></a>
 
                 </ul>
             </div>
-            <div id='footer-contact'>
+            <div id='footer-contact' className='footerUL'>
                 <ul>
-                    <a href='#'><li>TheGingerBlondieBakery@gmail.com</li></a>
-                    <a href='#'><li>+00 000 0000</li></a>
+                    <a href='mailto:TheGingerBlondieBakery@gmail.com'><li>TheGingerBlondieBakery@gmail.com</li></a>
+                    <a href='tel:+353862135386'><li>+353 86 213 5386</li></a>
 
                 </ul>
             </div>
